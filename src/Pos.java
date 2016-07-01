7d6a633abd5bb1e00b5cb8d8f66f9def8b214ae1
Vitor Pereira <rotiv695@gmail.com>
2016-07-01 19:21:42 +0100

    class that represents the coordinates on the board

---
+++ src/Pos.java
@@ -0,0 +1,16 @@
+/**
+ * This class represents the coordinates x and y on the chessboard
+ * @author Vitor
+ *
+ */
+
+public class Pos {
+	
+	public int x;
+	public int y;
+	
+	public Pos(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
